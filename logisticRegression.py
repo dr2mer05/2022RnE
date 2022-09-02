9719b93cafb645b5e9224e24097306300919f325
Heesung, Lee <93814044+dr2mer05@users.noreply.github.com>
2022-09-02 22:47:48 +0900

    Create logisticRegression.py

---
+++ logisticRegression.py
@@ -0,0 +1,108 @@
+import pandas as pd
+import csv
+import numpy as np
+
+x = []
+y = [0, 0, 0, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 0, 0, 0, 1, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1, 1, 1, 0, 1, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 0, 1, 0, 0, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 0, 1, 0, 0, 0, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 1, 1, 0, 1, 1, 0, 0, 1, 0, 0, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 1, 0, 1, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 0, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 1, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 1, 1, 1, 0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 1, 1, 0, 1, 1, 1, 0, 1, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 1, 0, 1, 0, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 0, 1, 1, 0, 1, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1, 0, 1, 1, 1, 0, 0, 0, 1, 1, 0, 1, 1, 1, 0, 1, 1, 0, 1, 0, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 1, 0, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 1, 1, 0, 0, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1, 0, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 0, 1, 1, 1, 0, 0, 1, 0, 1, 0, 1, 1, 1, 0, 1, 1, 0, 0, 0, 1, 0, 1, 0, 1, 1, 0, 0, 0, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 1, 0, 0, 1, 1, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 1, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0, 1, 1, 0, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 0, 0, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 0, 0, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 0, 1, 1, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 0, 0, 0, 0, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 0, 0, 0, 0, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 1, 0, 0, 1, 0, 1, 0, 0, 1, 1, 1, 1, 1, 0, 1, 0, 0, 1, 0, 0, 1, 0, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 0, 1, 0, 1, 1, 1, 1, 1, 0, 0, 1, 0, 0, 1, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 1, 0, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 0, 1, 1, 0, 0, 1, 0, 1, 1, 0, 1, 0, 0, 0, 0, 1, 1, 1, 0, 0, 1, 1, 0, 0, 0, 1, 0, 1, 1, 0, 0, 1, 0, 1, 0, 1, 1, 0, 0, 1, 1, 1]
+
+file_path = "/content/drive/Shareddrives/2022 R&E/data/Geant4/G4_data.csv"
+f = open(file_path,'r')
+rdr = csv.reader(f)
+for line in rdr:
+  x.append(line)            
+f.close()
+
+
+for i in range(len(x)):
+  for j in range(len(x[i])):
+    x[i][j] = int(x[i][j])
+
+for i in range(len(x)):
+  sum_x = sum(x[i])
+  for j in range(len(x[i])):
+    x[i][j] = x[i][j] / sum_x
+
+x = np.array(x)
+y = np.array(y)
+
+from sklearn.model_selection import train_test_split
+x_train, x_test, y_train, y_test = train_test_split(x, y, stratify=y)
+
+class SingleLayer:
+    
+    def __init__(self):
+        self.w = None
+        self.b = None
+        self.losses = []
+
+    def forpass(self, x):
+        z = np.sum(x * self.w) + self.b  # 직선 방정식을 계산합니다
+        return z
+
+    def backprop(self, x, err):
+        w_grad = x * err    # 가중치에 대한 그래디언트를 계산합니다
+        b_grad = 1 * err    # 절편에 대한 그래디언트를 계산합니다
+        return w_grad, b_grad
+
+    def activation(self, z):
+        z = np.clip(z, -100, None) # 안전한 np.exp() 계산을 위해
+        a = 1 / (1 + np.exp(-z))  # 시그모이드 계산
+        return a
+        
+    def fit(self, x, y, epochs=100):
+        self.w = np.ones(x.shape[1])               # 가중치를 초기화합니다.
+        self.b = 0                                 # 절편을 초기화합니다.
+        for i in range(epochs):                    # epochs만큼 반복합니다
+            loss = 0
+            # 인덱스를 섞습니다
+            indexes = np.random.permutation(np.arange(len(x)))
+            for i in indexes:                      # 모든 샘플에 대해 반복합니다
+                z = self.forpass(x[i])             # 정방향 계산
+                a = self.activation(z)             # 활성화 함수 적용
+                err = -(y[i] - a)                  # 오차 계산
+                w_grad, b_grad = self.backprop(x[i], err) # 역방향 계산
+                self.w -= w_grad                   # 가중치 업데이트
+                self.b -= b_grad                   # 절편 업데이트
+                # 안전한 로그 계산을 위해 클리핑한 후 손실을 누적합니다
+                a = np.clip(a, 1e-10, 1-1e-10)
+                loss += -(y[i]*np.log(a)+(1-y[i])*np.log(1-a))
+            # 에포크마다 평균 손실을 저장합니다
+            self.losses.append(loss/len(y))
+    
+    def predict(self, x):
+        z = [self.forpass(x_i) for x_i in x]     # 정방향 계산
+        # return np.array(z) > 0
+        a = self.activation(np.array(z))
+        return np.array(a)
+
+    def score(self, x, y):
+        return np.mean(self.predict(x) == y)
+        
+layer = SingleLayer()
+layer.fit(x_train, y_train)
+
+observed = []
+file_path = "/content/drive/Shareddrives/2022 R&E/data/observed/hist1_1.csv"
+f = open(file_path,'r')
+rdr = csv.reader(f)
+for line in rdr:
+  observed.append(line)            
+f.close()
+
+for i in range(len(observed)):
+  for j in range(len(observed[i])):
+    observed[i][j] = int(observed[i][j])
+
+for i in range(len(observed)):
+  sum_observed = sum(observed[i])
+  for j in range(len(observed[i])):
+    observed[i][j] = observed[i][j] / sum_observed
+
+observed = np.array(observed)
+
+layer = SingleLayer()
+layer.fit(x_train, y_train)
+ratio_list = list(layer.predict(observed))
+ratio_df = pd.DataFrame({'ratio':ratio_list})
+
+ratio_df.to_csv('ratio.csv')
